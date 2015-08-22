df297f5c9ea4c48b5e473405a9caf3b0b1a11d74
Matthew Suozzo <matthew.suozzo@gmail.com>
2015-08-22 17:44:07 -0400

    Added install_requires

--- setup.py
+++ setup.py
@@ -1,11 +1,17 @@
 #!/usr/bin/env python  #pylint: disable=missing-docstring
 
+import os
 try:
     from setuptools import setup
 except ImportError:
     from distutils.core import setup
 
 import kindle_api
+
+open_ = lambda fname: open(os.path.join(os.path.dirname(__file__), fname))
+
+with open_('requirements.txt') as f:
+    requires = f.read().splitlines()
 
 setup(name='Kindle API',
       version=kindle_api.__version__,
@@ -14,5 +20,6 @@ setup(name='Kindle API',
       author_email='iratus.litteris@gmail.com',
       url='https://github.com/msuozzo/kindle_api',
       packages=['kindle_api'],
+      install_requires=requires,
       license='MIT'
      )
