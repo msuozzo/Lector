ff802a0ab1f813267155f3c9b0c0f5303250b75b
Matthew Suozzo <msuozzo@google.com>
2015-11-22 10:24:42 -0500

    Fixed setup

--- lector/__init__.py
+++ lector/__init__.py
@@ -5,7 +5,7 @@ Lector
 Interface for Amazon Kindle Data
 """
 
-__version__ = '0.0.3a'
+__version__ = '0.0.3a1'
 
 from .reader import KindleAPIError, ConnectionError, InitError,\
         KindleBook, ReadingProgress, KindleCloudReaderAPI
--- setup.py
+++ setup.py
@@ -1,20 +1,32 @@
 #!/usr/bin/env python  #pylint: disable=missing-docstring
 
+import ast
 import os
+import re
 try:
     from setuptools import setup
 except ImportError:
     from distutils.core import setup
 
-import lector
+from setuptools import setup
 
-open_ = lambda fname: open(os.path.join(os.path.dirname(__file__), fname))
 
+def open_(fname, *args, **kwargs):
+    return open(os.path.join(os.path.dirname(__file__), fname), *args, **kwargs)
+
+# Extract the version string from lector/__init__.py
+_version_re = re.compile(r'__version__\s+=\s+(.*)')
+
+with open_('lector/__init__.py', 'rb') as f:
+    version = str(ast.literal_eval(_version_re.search(
+        f.read().decode('utf-8')).group(1)))
+
+# Extract the package requirements from requirements.txt
 with open_('requirements.txt') as f:
     requires = f.read().splitlines()
 
 setup(name='Lector',
-      version=lector.__version__,
+      version=version,
       description='An API for Amazon Kindle Data',
       author='Matthew Suozzo',
       author_email='iratus.litteris@gmail.com',
