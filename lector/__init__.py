5a2478df62ce0a2f811953ea4aabcda369e2c26b
Matthew Suozzo <msuozzo@google.com>
2015-11-22 08:56:36 -0500

    Added exports to lector module __init__

--- lector/__init__.py
+++ lector/__init__.py
@@ -5,4 +5,7 @@ Lector
 Interface for Amazon Kindle Data
 """
 
-__version__ = '0.0.2'
+__version__ = '0.0.3a'
+
+from .reader import KindleAPIError, ConnectionError, InitError,\
+        KindleBook, ReadingProgress, KindleCloudReaderAPI
